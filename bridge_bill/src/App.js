d5303542b8b7a104074b94666e6bb729ff0a3bda
kerrynx <kerrynxu@utexas.edu>
2024-03-18 19:42:19 -0500

    made home page -- kerryn

--- bridge_bill/src/App.js
+++ bridge_bill/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="top-left-text">
+        English
+        </a>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The billing assistant<br></br> your health deserves.
         </p>
+        <a
+          className="subheading">
+        Your Guide to Clear and Accurate Medical Billing 
+        </a>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Get Started
         </a>
       </header>
     </div>
+    
   );
 }
+
+
 
 export default App;
