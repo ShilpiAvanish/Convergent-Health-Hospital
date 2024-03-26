3ece3273250e212675e9d62fd4853984eb339316
UTShilpi <avanish.shilpi@utexas.edu>
2024-03-25 19:09:21 -0500

    vite

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import "./styles.css";
+import ImportPdf from "./ImportPDF";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a className="top-left-text">English</a>
+        <p>
+          The billing assistant<br></br> your health deserves.
+        </p>
+        <a className="subheading">
+          Your Guide to Clear and Accurate Medical Billing
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Get Started
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ImportPDF.tsx
@@ -0,0 +1,5 @@
+function ImportPdf(){
+    return <h1>Hello World</h1>;
+}
+
+export default ImportPdf;
